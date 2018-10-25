23f99b4c84b65acd21ff9db2fc9c57eeed858d1d
com48 <mukesh.mangal@daffodilsw.com>
2018-10-25 15:16:31 +0530

    login

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -6,35 +6,58 @@ import '../Login.css'
 @inject("params")
 @observer
 class LoginComponent extends Component {
-    handleNext=()=>{
+    constructor(props) {
+        super(props);
+        this.state = {username: "",
+            password:""
+
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        console.log("handleChange>>>>>>",event.target.value)
+        console.log("handleChange name>>>>>>",event.target.name)
+        let key=event.target.name
+        let value=event.target.value
+        this.setState({[key]: value});
+    }
+
+    handleSubmit(event) {
+        console.log("this handleSubmit state>>>>>>",this.state)
+        // alert('this.state>>>>>>>>>>' + this.state);
+        event.preventDefault();
+    }
+    updatePath=()=>{
         let {path,params}=this.props;
         let pathLength=path.length;
-        path.splice(0, pathLength, { path: "/signup"});
+        // path.splice(0, pathLength, { path: "/signup"});
+         path.push({path:"/signup"})
         params.reload=true;
-        // path.push({path:"/signup"})
     }
     render() {
         return (
             <div class="wrapper">
             <div class="container">
-                <form action="/action_page.php">
+                <form onSubmit={this.handleSubmit}>
                     <div class="row">
-
                         <div class="col">
                             <div class="hide-md-lg">
                                 <p>Or sign in manually:</p>
                             </div>
-                            <h2 style={{"text-align":"center"}}>Login  Manually</h2>
-                            <input type="text" name="username" placeholder="Username" required />
-                                <input type="password" name="password" placeholder="Password" required />
+                            <h2 style={{"text-align":"center"}}>Login</h2>
+                            <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChange} required/>
+                                <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
                                     <input type="submit" value="Login" />
                                 <div class="row">
                                     <div class="colfot">
-                                        <a href="" style={{"color": "black"}} onClick={this.handleNext}
-                                           className="btn">Sign up</a>
+                                        <button style={{"color": "black"}} onClick={this.updatePath}
+                                           className="btn">Sign up</button>
                                     </div>
                                     <div class="colfot">
-                                        <a href="" style={{"color":"black"}}onClick={this.handleNext} class="btn">Forgot password</a>
+                                        <button  style={{"color":"black"}}onClick={this.updatePath} class="btn">Forgot password</button>
                                     </div>
                             </div>
                         </div>
--- src/components/SignupComponent.js
+++ src/components/SignupComponent.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import {inject} from "mobx-react"
 import '../Login.css'
+
+@inject("path")
+@inject("params")
 class SignupComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {firstname: "",
+            lastname:"",
+            email:"",
+            password:""
+
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        console.log("handleChange>>>>>>",event.target.value)
+        console.log("handleChange name>>>>>>",event.target.name)
+        let key=event.target.name
+        let value=event.target.value
+        this.setState({[key]: value});
+    }
+
+    async handleSubmit(event) {
+        let {path,params}=this.props;
+        event.preventDefault();
+        path.pop()
+        params.reload=true;
+    }
     render() {
+        console.log("this.state>>>>>>>>>>>>>",this.state)
         return (
             <div class="wrapper">
                 <div class="container">
-                    <form action="/action_page.php">
+                    <form onSubmit={this.handleSubmit}>
                         <div class="row">
                             <div class="colSignup">
                                 <h2 style={{"text-align":"center"}}>Sign Up</h2>
@@ -13,11 +45,11 @@ class SignupComponent extends Component {
                                     <p>Or sign in manually:</p>
                                 </div>
 
-                                <input type="text" name="firstname" placeholder="First Name" required />
-                                <input type="text" name="lastname" placeholder="Lirst Name" required />
-                                <input type="email" name="email" placeholder="Email" required />
-                                <input type="password" name="password" placeholder="Password" required />
-                                <input type="submit" value="SignUp" />
+                                <input type="text" name="firstname" placeholder="First Name" value={this.state.firstname} onChange={this.handleChange} required />
+                                <input type="text" name="lastname" placeholder="Lirst Name" value={this.state.lastname} onChange={this.handleChange}/>
+                                <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
+                                <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
+                                <input type="submit" value="Submit" />
                             </div>
 
                         </div>
